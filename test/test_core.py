6b95c384a6f24fc4c0d5e879938867d987efc0fb
wagnerfns <wagnerfelidre@gmail.com>
2019-03-13 10:04:47 -0300

    test

---
+++ test/test_core.py
@@ -0,0 +1,32 @@
+import unittest
+from operations import Operations
+
+
+class TestMethods(unittest.TestCase):
+
+    def test_operation_addition(self):
+        operation = Operations(100, 100)
+        self.assertEqual(operation.setAddition(), 200, 'Incorrect result')
+
+    def test_operation_subtraction(self):
+        operation = Operations(100, 100)
+        self.assertEqual(operation.setSubtraction(), 0, 'Incorrect result')
+
+    def test_operation_multiplication(self):
+        operation = Operations(100, 1)
+        self.assertEqual(operation.setMultiplication(), 100, 'Incorrect result')
+
+    def test_operation_division(self):
+        operation = Operations(100,100)
+        self.assertEqual(operation.setDivision(), 1, 'Incorrect result')
+
+    def test_operation_root(self):
+        operation = Operations(100,1)
+        self.assertEqual(operation.setRoot(), 10, 'Incorrect result')
+
+    def test_operation_potentiation(self):
+        operation = Operations(10,1)
+        self.assertEqual(operation.setPotentiation(), 100, 'Incorrect result')
+
+if __name__ == '__main__':
+    unittest.main()
