86d92ca679b6c0eae8dd34d278c3738fe6d4eda1
Wagner Negrão <35495925+wagnerfns@users.noreply.github.com>
2019-03-18 15:38:14 -0300

    return float

--- operations.py
+++ operations.py
@@ -32,4 +32,4 @@ class Operations:
         return(self.value1 ** 2)
 
     def avg(self):
-        return(sum(self.value1)/len(self.value1))
+        return(float(sum(self.value1))/len(self.value1))
