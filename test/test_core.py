814f58b9b27b1295a097c5c96175d6f0545e00a1
Wagner Negrão <wagnerfelidre@gmail.com>
2019-05-28 08:43:34 -0300

    renomear teste

--- test/test_core.py
+++ test/test_core.py
@@ -6,31 +6,31 @@ class TestMethods(unittest.TestCase):
 
     def test_operation_addition(self):
         operation = Operations(100, 100)
-        self.assertEqual(operation.setAddition(), 200, 'Incorrect result')
+        self.assertEqual(operation.addition(), 200, 'Incorrect result')
 
     def test_operation_subtraction(self):
         operation = Operations(100, 100)
-        self.assertEqual(operation.setSubtraction(), 0, 'Incorrect result')
+        self.assertEqual(operation.subtraction(), 0, 'Incorrect result')
 
     def test_operation_multiplication(self):
         operation = Operations(100, 1)
-        self.assertEqual(operation.setMultiplication(), 100, 'Incorrect result')
+        self.assertEqual(operation.multiplication(), 100, 'Incorrect result')
 
     def test_operation_division(self):
         operation = Operations(100,100)
-        self.assertEqual(operation.setDivision(), 1, 'Incorrect result')
+        self.assertEqual(operation.division(), 1, 'Incorrect result')
 
     def test_operation_division_by_zero(self):
         operation = Operations(100,0)
-        self.assertEqual(operation.setDivision(), None, 'Incorrect result')
+        self.assertEqual(operation.division(), None, 'Incorrect result')
 
     def test_operation_root(self):
         operation = Operations(100,1)
-        self.assertEqual(operation.setRoot(), 10,  'Incorrect result')
+        self.assertEqual(operation.root(), 10,  'Incorrect result')
 
     def test_operation_potentiation(self):
         operation = Operations(10,1)
-        self.assertEqual(operation.setPotentiation(), 100,  'Incorrect result')
+        self.assertEqual(operation.potentiation(), 100,  'Incorrect result')
 
     def test_avg(self):
         operation = Operations([1,2,3,4], False)
